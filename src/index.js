0875a5f03d19b7f9a6d1ffc53c92a1483dec219c
LunaThomas69 <michaelt.4493@gmail.com>
2024-02-23 12:25:38 -0800

    working currency exchanger for all currency codes. Lacks units and drop down menu

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import CurrencyExchanger from './currency-exchanger.js';
 
 // Business Logic
 
-async function getExchangeRate(Currency) {
+async function getExchangeRate(Currency, Currency2) {
   const response = await CurrencyExchanger.getExchangeRate(Currency);
   if (response.result === "success") {
-    printElements(response, Currency);
+    printElements(response, Currency, Currency2);
   } else {
     printError(response, Currency);
   }
@@ -16,8 +16,8 @@ async function getExchangeRate(Currency) {
 
 // UI Logic
 
-function printElements(response, Currency) {
-  document.querySelector('#showResponse').innerText = `1 ${Currency} is ${response.conversion_rates.CZK} Units`;
+function printElements(response, Currency, Currency2) {
+  document.querySelector('#showResponse').innerText = `1 ${Currency} is ${response.conversion_rates[Currency2]} Units`;
 }
 
 function printError(error, Currency) {
@@ -28,8 +28,10 @@ function printError(error, Currency) {
 function handleFormSubmission(event) {
   event.preventDefault();
   const Currency = document.querySelector('#USD').value;
+  const Currency2 = document.querySelector('#Currency2').value;
   document.querySelector('#USD').value = null;
-  getExchangeRate(Currency);
+  document.querySelector('#Currency2').value = null;
+  getExchangeRate(Currency, Currency2);
 }
 
 window.addEventListener("load", function() {
